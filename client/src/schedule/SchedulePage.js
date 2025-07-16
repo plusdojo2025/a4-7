12fa5efe29614ccb65a248d2d01e784573a25527
plusdojo2025-furuta <huruta-masaki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-16 15:42:21 +0900

    SchedulePage.jsの追加とApp.jsに<BrowserRouter>の追加

---
+++ client/src/schedule/SchedulePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class SchedulePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Schedule Page</h1>
+                <nav>
+                    <ul>
+                        <li><a href="/">予定表</a></li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
