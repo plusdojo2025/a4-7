64325422dda7ac2f4038030422785528f0444a3a
plusdojo2025-furuta <huruta-masaki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-17 10:37:11 +0900

    担当Repositoryの作成

---
+++ server/src/main/java/com/example/demo/repository/ColumnsRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Columns;
+import com.example.demo.entity.PrivateSchedules;
+
+public interface ColumnsRepository extends JpaRepository<Columns, Integer> {
+	List<PrivateSchedules> findByUserIdAndVacationId(String userId, int vacationId);
+}
