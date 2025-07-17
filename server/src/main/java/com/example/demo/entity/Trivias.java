a4b641098f0e4e61bdd3651d5b4c4a0dda888a1b
plusdojo2025-numata <numata-kanon-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-17 12:01:00 +0900

    担当Entity

---
+++ server/src/main/java/com/example/demo/entity/Trivias.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="trivias")
+public class Trivias {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private Date date;
+	private String content;
+	
+	
+}
