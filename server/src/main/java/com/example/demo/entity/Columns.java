850ed444f7becced63cc2038124d10b174e8f2f7
E3fujita <fujita-ryusei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-17 11:49:11 +0900

    担当entity作成

--- server/src/main/java/com/example/demo/entity/Columns.java
+++ server/src/main/java/com/example/demo/entity/Columns.java
@@ -1,5 +1,27 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "columns")
 public class Columns {
+
+	@Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Integer id;
+	    private Integer userId;
+	    private String columnTitle;
+	    private Integer position;
+	    private Integer vacationId;
 
 }
